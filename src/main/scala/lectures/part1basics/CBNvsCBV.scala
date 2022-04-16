c4595ba71be103bdcad6f6cd8a8fb77e6d565510
Shaw Lu <shawlu95@gmail.com>
2022-04-16 00:28:19 -0700

    Call by value vs name

---
+++ src/main/scala/lectures/part1basics/CBNvsCBV.scala
@@ -0,0 +1,30 @@
+package lectures.part1basics
+
+object CBNvsCBV extends App {
+  def calledByValue(x: Long): Unit = {
+    println("by value " + x)
+    println("by value " + x)
+  }
+
+  // =>: tell compiler it is called by name
+  def calledByName(x: => Long): Unit = {
+    println("by name " + x)
+    println("by name " + x)
+  }
+
+  // param is computed before, the value is passed in
+  calledByValue(System.nanoTime())
+
+  // expression is passed literally into function
+  // and evaluated every time inside function when referenced
+  calledByName(System.nanoTime())
+
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = println(x)
+
+  // crashes with stack overflow
+  // printFirst(infinite(), 34)
+
+  // y is not used, so y is never evaluated. No crash
+  printFirst(34, infinite())
+}
