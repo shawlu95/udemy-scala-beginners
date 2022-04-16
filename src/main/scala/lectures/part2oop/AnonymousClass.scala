cc83fec9825b8fcf7e3b545250fc8b6e0afc164c
Shaw Lu <shawlu95@gmail.com>
2022-04-16 16:37:06 -0700

    Create AnonymousClass.scala

---
+++ src/main/scala/lectures/part2oop/AnonymousClass.scala
@@ -0,0 +1,28 @@
+package lectures.part2oop
+
+/* Works for class and trait, abstract and concrete */
+object AnonymousClass extends App {
+  abstract class Animal {
+    def eat: Unit
+  }
+
+  // declare an anonymous class and instantiate
+  val funny: Animal = new Animal {
+    override def eat: Unit = println("yoo")
+  }
+  // class lectures.part2oop.AnonymousClass$$anon$1
+  println(funny.getClass)
+
+  // doesn't have to be abstract
+  class Person(name: String) {
+    def greet: Unit = println(s"Hi my name is $name")
+  }
+
+  // pass in required constructor params
+  val shaw = new Person(name = "shaw") {
+    override def greet: Unit = println("works fine")
+  }
+  // class lectures.part2oop.AnonymousClass$$anon$2
+  println(shaw.getClass)
+
+}
