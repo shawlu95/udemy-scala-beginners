9f03c80d52a235f689ec1f6bc394926687c3f7d7
Shaw Lu <shawlu95@gmail.com>
2022-04-16 16:29:16 -0700

    Generics

--- src/main/scala/lectures/part2oop/AbstractExercises.scala
+++ src/main/scala/lectures/part2oop/AbstractExercises.scala
@@ -1,6 +1,10 @@
 package lectures.part2oop
 
-abstract class MyList {
+import math.Fractional.Implicits.infixFractionalOps
+import math.Integral.Implicits.infixIntegralOps
+import math.Numeric.Implicits.infixNumericOps
+
+abstract class MyList[+A] {
   /*
   * head = first element of the list
   * tail = remainder of the list
@@ -8,32 +12,35 @@ abstract class MyList {
   * add(int) => new list with the element added
   * toString => a string representation */
 
-  def head: Int
-  def tail: MyList
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
   def printElements: String // will call subclass's (MyList) printElements method
   override def toString: String = "[" + printElements + "]"
 }
 
 // object can extends classes!
-object Empty extends MyList {
-  def head: Int = throw new NoSuchElementException
-  def tail: MyList = throw new NoSuchElementException
+// Nothing is a proper substitute for any type
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(element: Int): MyList = new Cons(element, Empty)
+
+  // B is a supertype of nothing, always true
+  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  def head: Int = h
-  def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
+  def tail: MyList[A] = t
   def isEmpty: Boolean = false
-  def add(element: Int): MyList = new Cons(element, this)
+  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
 
-  def printElements: String =
-    if (t.isEmpty) "" + h
-    else h + " " + t.printElements
+    def printElements: String =
+      if (t.isEmpty) "" + h
+      else s"$h ${t.printElements}"
 }
 
 object ListTest {
@@ -47,5 +54,11 @@ object ListTest {
     println(long.head)
     println(long.isEmpty)
     println(long.toString)
+
+    val listOfInt: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+    val listOfStrings: MyList[String] = new Cons("a", new Cons("b", new Cons("c", Empty)))
+
+    println(listOfInt.toString)
+    println(listOfStrings.toString)
   }
 }
---
+++ src/main/scala/lectures/part2oop/Generics.scala
@@ -0,0 +1,75 @@
+package lectures.part2oop
+
+object Generics {
+  class MyList[+A] { // covariant collection
+    // use type "A" inside the class! A generic class
+    
+    // covariant type A occurs in contravariant position in type A of parameter element
+    // def add(element: A): MyList[A] = ???
+    
+    // fix: add a dog to cat list, the list become a list of animal
+    def add[B >: A](element: B): MyList[B] = ???
+  }
+  
+  // can have multiple 
+  class MyMap[Key, Value] 
+  
+  // trait can also be parametrized
+  trait Set[B]
+  
+  object MyList {
+    // generic method
+    def empty[A]: MyList[A] = new MyList[A]
+  }
+
+  // Variance problem: does list of cat extends list of animal
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  // 1. Covariance: list of Cat extends list of Animals
+  class CovariantList[+A]
+  
+  // 2. Invariant list
+  class InvariantList[A]
+
+  // 3. contravariant list
+  class ContraVariantList[-A]
+  class Trainer[-A]
+  
+  // bounded type 
+  // A must be sub-type of Animal
+  class Cage[A <: Animal](animal: A)
+  class Car
+  // accept super type
+  class Meal[A >: Animal](animal: A)
+  
+  
+  def main(args: Array[String]): Unit = { 
+    
+    val listOfIntegers = new MyList[Int]
+    val listOfStrings = new MyList[String]
+    
+    val emptyIntList = MyList.empty[Int]
+    
+    // covariance example
+    val animal: Animal = new Cat
+    val animalList: CovariantList[Animal] = new CovariantList[Cat]
+    // bad, dog will pollute cat list
+    // animalList.add(new Dog)
+    
+    // invariance example
+    val invariant: InvariantList[Animal] = new InvariantList[Animal]
+    
+    // contra-variance example: not intuitive
+    val contraList: ContraVariantList[Cat] = new ContraVariantList[Animal]
+    // want a trainer of cat, provide a trainer of any animal (more reasonable)
+    val trainer: Trainer[Cat] = new Trainer[Animal]
+    
+    // Cage can only contain animal subclass
+    val cage = new Cage(new Dog)
+    // val cage = new Cage(new Car) // not allowed
+    
+  }
+
+}
