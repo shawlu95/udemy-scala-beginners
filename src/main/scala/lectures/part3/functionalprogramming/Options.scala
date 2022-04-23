a11238f11bb35f9836e3dca02e20859c3718f5f1
Shaw Lu <shawlu95@gmail.com>
2022-04-22 22:42:58 -0700

    Options exercise

--- src/main/scala/lectures/part3/functionalprogramming/Options.scala
+++ src/main/scala/lectures/part3/functionalprogramming/Options.scala
@@ -1,5 +1,7 @@
 package lectures.part3.functionalprogramming
 
+import scala.util.Random
+
 // Avoid null pointer exception!
 // Never do null-check manually
 object Options extends App {
@@ -36,4 +38,67 @@ object Options extends App {
   println(firstOption.map(_ * 2)) // returns Some(4)
   println(firstOption.filter(x => x > 10)) // becomes None
   println(firstOption.flatMap(x => Option(x * 10))) // returns Some(40)
+
+  /* Exercise
+  */
+  val config: Map[String, String] = Map(
+    "host" -> "127.0.0.1",
+    "port" -> "80"
+  )
+
+  class Connection {
+    def connect = "connected"
+  }
+
+  object Connection {
+    val random = new Random(System.nanoTime())
+    def apply(host: String, port: String): Option[Connection] =
+      if (random.nextBoolean()) Some(new Connection)
+      else None
+  }
+
+  // API might or might not return connection
+  // try until a connection is established
+  /*
+  if (h != null)
+    if (p != null)
+      return Connection(h, p)
+  return null
+  */
+  val host = config.get("host")
+  val port = config.get("port")
+  val connection = host.flatMap(h => port.flatMap(p => Connection(h, p)))
+
+  /*
+  if (c != null)
+    return c.connect
+  return null
+  */
+  val status = connection.map(c => c.connect)
+
+  /*
+  if (status == nul) println(None
+  else print Some(status.get)
+  */
+  println(status)
+
+  // if (status != null) print(status
+  status.foreach(println)
+
+  // chain all steps, careful with nesting here
+  println("chain all steps")
+  config.get("host")
+    .flatMap(host => config.get("port")
+      .flatMap(port => Connection(host, port))
+      .map(connection => connection.connect))
+    .foreach(println)
+
+  // for-comprehension: either host, port, conn is none, return none
+  val yolo = for {
+    host <- config.get("host")
+    port <- config.get("port")
+    connection <- Connection(host, port)
+  } yield connection.connect
+  println("for-comprehension")
+  yolo.foreach(println)
 }
